831616022d09971037d5d47baa9fdc29878e659e
andres <andres.nyc.col@gmail.com>
2024-12-28 13:28:08 -0500

    Correction

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
 // Write your solution in this file!
-const employee = [
-    "name: Sam",
-    `streetAddress: 11 Broadway`
-]
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway"
+};
 
-function updateEmployeeWithKeyAndValue (obj,key,value) {
-const newEmployee = {
-    ...employee };
-    newEmployee [key] = value
-    return newEmployee 
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {
+        ...obj // Spread the properties of the original object
+    };
+    newEmployee[key] = value; // Add/Update the key-value pair
+    return newEmployee; // Return the new object
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue (key,value) {
-    const Employee = 
-        Employee [key] = value
-        return Employee 
-    }
-        
-
-function deleteFromEmployeeByKey (employee, key){
-    const newemployee = {
-        ...employee
-    }
-    delete [key];
-    return [key];
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; // Directly update the key-value pair on the original object
+    return obj; // Return the updated object
 }
-  
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {
+        ...employee // Spread the properties of the original object
+    };
+    delete newEmployee[key]; // Remove the key-value pair from the new object
+    return newEmployee; // Return the new object
+}
+
 function destructivelyDeleteFromEmployeeByKey(obj, key) {
-    delete obj[key]; 
-    return obj;
-  }  
+    delete obj[key]; // Directly delete the key-value pair from the original object
+    return obj; // Return the updated object
+}
 
-  console.log(employee);
+console.log(employee);
